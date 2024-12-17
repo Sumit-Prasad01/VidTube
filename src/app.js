5ff135aab67ea17a42867076fcfbd8bc3002db94
Sumit Prasad <sumitprasadd.wb24@gmail.com>
2024-12-17 22:19:24 +0530

    cloudinary configured

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
 app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
@@ -12,6 +14,7 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+app.use(cookieParser)
 
 //import routes
 
